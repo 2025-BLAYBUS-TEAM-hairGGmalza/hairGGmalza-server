e9d5b8037b287afaf1368e8ad1cebcfa31908f37
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-12 23:36:34 +0900

    Initial commit

---
+++ hairgg/src/main/java/hair/hairgg/HairggApplication.java
@@ -0,0 +1,13 @@
+package hair.hairgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairggApplication.class, args);
+	}
+
+}
---
+++ hairgg/src/test/java/hair/hairgg/HairggApplicationTests.java
@@ -0,0 +1,13 @@
+package hair.hairgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
