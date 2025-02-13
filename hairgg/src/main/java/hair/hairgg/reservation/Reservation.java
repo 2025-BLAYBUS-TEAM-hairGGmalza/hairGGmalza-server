6c7f0a12dc558004de92d0b1c38a09c20c7f0096
Miging <dnfldpden32@gmail.com>
2025-02-14 02:30:26 +0900

    feat: entity및 dto 설계

--- hairgg/src/main/java/hair/hairgg/reservation/Reservation.java
+++ hairgg/src/main/java/hair/hairgg/reservation/Reservation.java
@@ -1,14 +1,55 @@
 package hair.hairgg.reservation;
 
+import java.time.LocalDateTime;
+
+import hair.hairgg.designer.domain.Designer;
+import hair.hairgg.designer.domain.MeetingType;
+import hair.hairgg.member.Member;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
 @Getter
 public class Reservation {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservationId")
     private long id;
+    @Column(nullable = false)
+    private LocalDateTime reservationDate;
+    @Column(nullable = false)
+    private ReservationState reservationState;
+    @Column(nullable = false)
+    private MeetingType meetingType;
+    @Column(nullable = false)
+    private int price;
+    @Column
+    private LocalDateTime paymentAt;
+    @Column
+    private PaymentMethod paymentMethod;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "designerId")
+    private Designer designer;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId")
+    private Member member;
+
 }
--- hairgg/src/main/java/hair/hairgg/reservation/ReservationDto.java
+++ hairgg/src/main/java/hair/hairgg/reservation/ReservationDto.java
@@ -1,7 +1,26 @@
 package hair.hairgg.reservation;
 
+import java.time.LocalDateTime;
+
+import hair.hairgg.designer.domain.MeetingType;
+
 public class ReservationDto{
+	public record ReservationRequest(
+		//TODO: 추후 토큰에서 가져오기
+		Long memberId,
+		Long designerId,
+		MeetingType meetingType,
+		//yyyy-MM-dd HH:mm
+		LocalDateTime reservationDate
+	){}
+
 	public record ReservationInfo(
-		Long userId
+		Long id,
+		Long memberId,
+		Long designerId,
+		ReservationState state,
+		MeetingType meetingType,
+		int price,
+		LocalDateTime reservationDate
 	){}
 }
