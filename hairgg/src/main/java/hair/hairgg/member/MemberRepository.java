81690a63e11f61b1ed9924129df8a332dd335859
Miging <dnfldpden32@gmail.com>
2025-02-14 02:31:27 +0900

    feat: 로직 확인을 위한 임시 repository 구현

---
+++ hairgg/src/main/java/hair/hairgg/member/MemberRepository.java
@@ -0,0 +1,9 @@
+package hair.hairgg.member;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	Optional<Member> findById(Long id);
+}
