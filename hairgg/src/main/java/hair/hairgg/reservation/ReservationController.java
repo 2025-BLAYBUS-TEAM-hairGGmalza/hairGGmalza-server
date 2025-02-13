914c50fa324d42ffab67992569478ac9b374d009
Miging <dnfldpden32@gmail.com>
2025-02-14 02:30:43 +0900

    feat: url 매핑

--- hairgg/src/main/java/hair/hairgg/reservation/ReservationController.java
+++ hairgg/src/main/java/hair/hairgg/reservation/ReservationController.java
@@ -1,25 +1,26 @@
 package hair.hairgg.reservation;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import hair.hairgg.apiPayLoad.ApiResponse;
 import hair.hairgg.exception.ErrorCode;
 import hair.hairgg.exception.custom.ReservationError;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/reservation")
+@RequiredArgsConstructor
 public class ReservationController {
 
-	//응답으로 <>안에 DTO 넣어주기
-	@GetMapping
-	public ApiResponse<ReservationDto.ReservationInfo> getReservation() {
+	private final ReservationService reservationService;
 
-		//에러 던지려면
-		// throw new ReservationError(ErrorCode.RESERVATION_NOT_FOUND);
-
-		//기본 응답
-		return ApiResponse.success("예약 조회 성공", new ReservationDto.ReservationInfo(1L));
+	@PostMapping
+	public ApiResponse<ReservationDto.ReservationInfo> createReservation(@RequestBody ReservationDto.ReservationRequest request) {
+		Reservation newReservation=reservationService.createReservation(request);
+		return ApiResponse.success("예약 생성 성공", ReservationConverter.toReservationInfo(newReservation));
 	}
 }
