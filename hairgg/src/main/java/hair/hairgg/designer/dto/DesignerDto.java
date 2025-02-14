165a4c0b75033575ce67b17a977978979898daca
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-14 23:58:47 +0900

    chore : Designer 응답 구조

---
+++ hairgg/src/main/java/hair/hairgg/designer/dto/DesignerDto.java
@@ -0,0 +1,37 @@
+package hair.hairgg.designer.dto;
+
+import hair.hairgg.designer.domain.Major;
+import hair.hairgg.designer.domain.MeetingType;
+import hair.hairgg.designer.domain.Region;
+import lombok.Builder;
+
+import java.util.List;
+
+public class DesignerDto {
+
+    @Builder
+    public record DesignerInfo(
+            Long designerId,
+            String name,
+            Region region,
+            String address,
+            String profile,
+            String description,
+            int offlinePrice,
+            int onlinePrice,
+            MeetingType meetingType,
+            List<Major> majors
+    ) {
+    }
+
+    @Builder
+    public record DesignerInfos(
+            List<DesignerInfo> designerInfos,
+            Integer listSize,
+            Integer totalPage,
+            Long totalElements,
+            Boolean isFirst,
+            Boolean isLast
+    ) {
+    }
+}
