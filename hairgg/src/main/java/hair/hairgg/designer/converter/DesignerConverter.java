77ab3153c0065b506a1ee2ee20d1ef9ab26edfb0
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-14 23:58:21 +0900

    chore : Designer 응답 변환

---
+++ hairgg/src/main/java/hair/hairgg/designer/converter/DesignerConverter.java
@@ -0,0 +1,45 @@
+package hair.hairgg.designer.converter;
+
+import hair.hairgg.designer.domain.Designer;
+import hair.hairgg.designer.domain.DesignerMajor;
+import hair.hairgg.designer.domain.Major;
+import hair.hairgg.designer.dto.DesignerDto.DesignerInfo;
+import hair.hairgg.designer.dto.DesignerDto.DesignerInfos;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public class DesignerConverter {
+
+    public static DesignerInfo toDesignerInfo(Designer designer) {
+        List<Major> majors = designer.getDesignerMajors().stream().map(DesignerMajor::getMajor).toList();
+
+        return DesignerInfo.builder()
+                .designerId(designer.getId())
+                .name(designer.getName())
+                .region(designer.getRegion())
+                .address(designer.getAddress())
+                .profile(designer.getProfile())
+                .description(designer.getDescription())
+                .offlinePrice(designer.getOfflinePrice())
+                .onlinePrice(designer.getOnlinePrice())
+                .meetingType(designer.getMeetingType())
+                .majors(majors)
+                .build();
+    }
+
+    public static DesignerInfos toDesignerInfoList(Page<Designer> designers) {
+        List<DesignerInfo> designerInfoList = designers.stream()
+                .map(DesignerConverter::toDesignerInfo)
+                .toList();
+
+        return DesignerInfos.builder()
+                .designerInfos(designerInfoList)
+                .listSize(designerInfoList.size())
+                .totalPage(designers.getTotalPages())
+                .totalElements(designers.getTotalElements())
+                .isFirst(designers.isFirst())
+                .isLast(designers.isLast())
+                .build();
+    }
+}
