b67679a7ec6896ab6465c3d034e58719b2774832
Miging <dnfldpden32@gmail.com>
2025-02-15 01:48:41 +0900

    refactor: validate 추가

--- hairgg/src/main/java/hair/hairgg/reservation/ReservationController.java
+++ hairgg/src/main/java/hair/hairgg/reservation/ReservationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import hair.hairgg.apiPayLoad.ApiResponse;
 import hair.hairgg.exception.ErrorCode;
 import hair.hairgg.exception.custom.ReservationError;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,7 +20,7 @@ public class ReservationController {
 	private final ReservationService reservationService;
 
 	@PostMapping
-	public ApiResponse<ReservationDto.ReservationInfo> createReservation(@RequestBody ReservationDto.ReservationRequest request) {
+	public ApiResponse<ReservationDto.ReservationInfo> createReservation(@Valid @RequestBody ReservationDto.ReservationRequest request) {
 		Reservation newReservation=reservationService.createReservation(request);
 		return ApiResponse.success("예약 생성 성공", ReservationConverter.toReservationInfo(newReservation));
 	}
--- hairgg/src/main/java/hair/hairgg/reservation/ReservationDto.java
+++ hairgg/src/main/java/hair/hairgg/reservation/ReservationDto.java
@@ -2,15 +2,20 @@ package hair.hairgg.reservation;
 
 import java.time.LocalDateTime;
 
+
 import hair.hairgg.designer.domain.MeetingType;
+import jakarta.validation.constraints.NotNull;
 
 public class ReservationDto{
 	public record ReservationRequest(
 		//TODO: 추후 토큰에서 가져오기
+		@NotNull(message = "회원 아이디는 필수 입력값입니다.")
 		Long memberId,
+		@NotNull(message = "디자이너 아이디는 필수 입력값입니다.")
 		Long designerId,
+		@NotNull(message = "예약 타입은 필수 입력값입니다.")
 		MeetingType meetingType,
-		//yyyy-MM-dd HH:mm
+		@NotNull(message = "예약 날짜는 필수 입력값입니다.")
 		LocalDateTime reservationDate
 	){}
 
