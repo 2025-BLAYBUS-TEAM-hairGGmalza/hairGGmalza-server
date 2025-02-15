0971c5c212b871f71714eb246e0ecc940a0c10de
Miging <dnfldpden32@gmail.com>
2025-02-16 00:07:17 +0900

    fix: merge후 오류 수정

--- hairgg/src/test/java/hair/hairgg/mock/designer/MockDesignerService.java
+++ hairgg/src/test/java/hair/hairgg/mock/designer/MockDesignerService.java
@@ -1,9 +1,11 @@
 package hair.hairgg.mock.designer;
 
+import org.springframework.data.domain.Page;
+
 import hair.hairgg.designer.domain.Designer;
-import hair.hairgg.designer.domain.DesignerService;
 import hair.hairgg.designer.domain.MeetingType;
 import hair.hairgg.designer.domain.Region;
+import hair.hairgg.designer.service.DesignerService;
 import hair.hairgg.exception.ErrorCode;
 import hair.hairgg.exception.GeneralException;
 
@@ -11,8 +13,13 @@ public class MockDesignerService implements DesignerService {
 	@Override
 	public Designer getDesignerById(Long id) {
 		if (id == 1) {
-			return new Designer(1L, "김디자이너", Region.SEOUL, "서울시 강남구", "fdsd", "fdsf", 1000, 2000, MeetingType.BOTH);
+			return new Designer(1L, "김디자이너", Region.서울전체, "서울시 강남구", "fdsd", "fdsf", 1000, 2000, MeetingType.BOTH,null);
 		}
 		throw new GeneralException(ErrorCode.DESIGNER_NOT_FOUND);
+	}
+
+	@Override
+	public Page<Designer> getDesigners(Integer page) {
+		return null;
 	}
 }
