03811d16a59cee11bf9f355eb7b6ad3b12e88138
Miging <dnfldpden32@gmail.com>
2025-02-15 19:57:04 +0900

    test:테스트코드 작성

--- hairgg/src/main/java/hair/hairgg/reservation/ReservationController.java
+++ hairgg/src/main/java/hair/hairgg/reservation/ReservationController.java
@@ -1,14 +1,11 @@
 package hair.hairgg.reservation;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import hair.hairgg.apiPayLoad.ApiResponse;
-import hair.hairgg.exception.ErrorCode;
-import hair.hairgg.exception.custom.ReservationError;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -17,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReservationController {
 
-	private final ReservationService reservationService;
+	private final ReservationServiceImpl reservationService;
 
 	@PostMapping
 	public ApiResponse<ReservationDto.ReservationInfo> createReservation(
---
+++ hairgg/src/test/java/hair/hairgg/reservation/ReservationTest.java
@@ -0,0 +1,88 @@
+package hair.hairgg.reservation;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hair.hairgg.designer.domain.MeetingType;
+import hair.hairgg.exception.custom.ReservationError;
+import hair.hairgg.mock.designer.MockDesignerService;
+import hair.hairgg.mock.member.MockMemberService;
+import hair.hairgg.mock.reservation.MockReservationRepository;
+
+public class ReservationTest {
+
+	private ReservationService reservationService = new ReservationServiceImpl(
+		new MyMockReservationRepository(),
+		new MockMemberService(),
+		new MockDesignerService()
+	);
+
+	private static class MyMockReservationRepository extends MockReservationRepository {
+		@Override
+		public List<Reservation> findByDesigner_IdAndReservationDateAndReservationStateIn(Long designerId,
+			LocalDateTime reservationDate, List<ReservationState> reservationStates) {
+
+			if(designerId == 1L && reservationDate.equals(LocalDateTime.parse("2021-08-01T10:00"))){
+				return List.of(Reservation.builder()
+					.reservationDate(LocalDateTime.parse("2021-08-01T10:00"))
+					.build());
+			}
+
+			return List.of();
+		}
+
+		@Override
+		public <S extends Reservation> S save(S entity) {
+			return entity;
+		}
+	}
+
+	@Test
+	void reservation_validation_test(){
+		assertThatThrownBy(() -> {
+			Reservation.builder().build();
+		}).isInstanceOf(Exception.class);
+	}
+
+	@Test
+	void 예약하기(){
+		//given
+		Long designerId = 1L;
+		Long memberId = 1L;
+		MeetingType meetingType = MeetingType.ONLINE;
+		LocalDateTime reservationDate = LocalDateTime.parse("2025-08-01T10:00");
+
+		var request = new ReservationDto.ReservationRequest(
+			designerId, memberId, meetingType, reservationDate
+		);
+
+		//when
+		Reservation reservation = reservationService.createReservation(request);
+
+		//then
+		assertThat(reservation).isNotNull();
+	}
+
+	@Test
+	void 예약시간_중복시_에러반환(){
+		// givenr
+		Long designerId = 1L;
+		Long memberId = 1L;
+		MeetingType meetingType = MeetingType.ONLINE;
+		LocalDateTime reservationDate = LocalDateTime.parse("2021-08-01T10:00");
+
+		var request = new ReservationDto.ReservationRequest(
+			designerId, memberId, meetingType, reservationDate
+		);
+
+		//when - then
+		assertThatThrownBy(() -> {
+			reservationService.createReservation(request);
+		}).isInstanceOf(ReservationError.class);
+	}
+}
