e9ef658bcc6b7204b0fbb80be66fd860fc591893
Miging <dnfldpden32@gmail.com>
2025-02-16 22:33:38 +0900

    feat: 예약 정보 조회 url 매핑

--- hairgg/src/main/java/hair/hairgg/reservation/ReservationController.java
+++ hairgg/src/main/java/hair/hairgg/reservation/ReservationController.java
@@ -1,11 +1,17 @@
 package hair.hairgg.reservation;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import hair.hairgg.apiPayLoad.ApiResponse;
+import hair.hairgg.reservation.domain.Reservation;
+import hair.hairgg.reservation.service.ReservationServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -21,5 +27,11 @@ public class ReservationController {
 		@Valid @RequestBody ReservationDto.ReservationRequest request) {
 		Reservation newReservation = reservationService.createReservation(request);
 		return ApiResponse.success("예약 생성 성공", ReservationConverter.toReservationInfo(newReservation));
+	}
+
+	@GetMapping
+	public ApiResponse<List<ReservationDto.ReservationDetailInfo>> getReservationByMemberId(@RequestParam Long memberId) {//TODO: 추후 토큰에서 가져오기
+		List<Reservation> reservations = reservationService.getReservationByMemberId(memberId);
+		return ApiResponse.success("예약 조회 성공",ReservationConverter.toReservationDetailInfoList(reservations));
 	}
 }
