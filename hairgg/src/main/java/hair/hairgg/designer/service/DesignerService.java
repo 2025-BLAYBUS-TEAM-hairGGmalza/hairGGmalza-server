b4d8c8d1e72102d6ebad003ad005589be9a321cd
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-17 19:46:17 +0900

    feat : query dsl 필터링 서비스 로직 구현

--- hairgg/src/main/java/hair/hairgg/designer/service/DesignerService.java
+++ hairgg/src/main/java/hair/hairgg/designer/service/DesignerService.java
@@ -1,10 +1,13 @@
 package hair.hairgg.designer.service;
 
 import hair.hairgg.designer.domain.Designer;
+import hair.hairgg.designer.dto.SearchFilterDto;
 import org.springframework.data.domain.Page;
 
 public interface DesignerService {
     Designer getDesignerById(Long designerId);
 
     Page<Designer> getDesigners(Integer page);
+
+    Page<Designer> getDesignersWithFilter(Integer page, SearchFilterDto filter);
 }
