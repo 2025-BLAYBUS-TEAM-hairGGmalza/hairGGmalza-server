e770fb04a8054b9ab911f1aa3b3c9616e0170f8c
Miging <dnfldpden32@gmail.com>
2025-02-17 23:52:16 +0900

    feat: 테스트 컨트롤러 추가

---
+++ hairgg/src/main/java/hair/hairgg/test/TestController.java
@@ -0,0 +1,13 @@
+package hair.hairgg.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+	public String test() {
+		return "test";
+	}
+}
