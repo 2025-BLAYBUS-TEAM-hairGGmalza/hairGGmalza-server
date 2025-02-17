c1eb0109a5469a4972e2f8b5f4c84307209741d3
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-17 19:47:22 +0900

    feat : 기존 디자이너 목록 조회 api -> 디자이너 목록 필터링 조회 api로 변경

--- hairgg/src/main/java/hair/hairgg/designer/controller/DesignerController.java
+++ hairgg/src/main/java/hair/hairgg/designer/controller/DesignerController.java
@@ -4,6 +4,7 @@ import hair.hairgg.apiPayLoad.ApiResponse;
 import hair.hairgg.designer.converter.DesignerConverter;
 import hair.hairgg.designer.domain.Designer;
 import hair.hairgg.designer.dto.DesignerDto.DesignerInfos;
+import hair.hairgg.designer.dto.SearchFilterDto;
 import hair.hairgg.designer.service.DesignerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -26,9 +27,11 @@ public class DesignerController {
     }
 
     @GetMapping("")
-    public ApiResponse<DesignerInfos> getAllDesigners(@RequestParam(name = "page", defaultValue = "0") Integer page) {
-        Page<Designer> designers = designerService.getDesigners(page);
+    public ApiResponse<DesignerInfos> getAllDesigners(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestBody SearchFilterDto filter) {
+        Page<Designer> designers = designerService.getDesignersWithFilter(page, filter);
 
-        return ApiResponse.success("모든 디자이너 조회 성공", DesignerConverter.toDesignerInfoList(designers));
+        return ApiResponse.success("디자이너 목록 조회 성공", DesignerConverter.toDesignerInfoList(designers));
     }
 }
