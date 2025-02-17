5156008c440b8b7564f318824fffad3b0bb0da81
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-17 19:44:42 +0900

    chore : lazy 로딩 -> eager 로딩으로 수정

--- hairgg/src/main/java/hair/hairgg/designer/domain/Designer.java
+++ hairgg/src/main/java/hair/hairgg/designer/domain/Designer.java
@@ -12,34 +12,34 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Designer {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "designerId")
-	private Long id;
-	@Column(nullable = false, length = 10)
-	private String name;
-	@Enumerated(EnumType.ORDINAL)
-	private Region region;
-	@Column(nullable = false)
-	private String address;
-	@Column(nullable = false)
-	private String profile;
-	@Column(nullable = false)
-	private String description;
-	@Column(nullable = false)
-	private int offlinePrice;
-	@Column(nullable = false)
-	private int onlinePrice;
-	@Enumerated(EnumType.ORDINAL)
-	private MeetingType meetingType;
-	@OneToMany(mappedBy = "designer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@Builder.Default
-	private List<DesignerMajor> designerMajors = new ArrayList<>();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "designerId")
+    private Long id;
+    @Column(nullable = false, length = 10)
+    private String name;
+    @Enumerated(EnumType.ORDINAL)
+    private Region region;
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private String profile;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private int offlinePrice;
+    @Column(nullable = false)
+    private int onlinePrice;
+    @Enumerated(EnumType.ORDINAL)
+    private MeetingType meetingType;
+    @OneToMany(mappedBy = "designer", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<DesignerMajor> designerMajors = new ArrayList<>();
 
-	public void addDesignerMajor(DesignerMajor designerMajor) {
-		this.designerMajors.add(designerMajor);
-		designerMajor.updateDesigner(this);
-	}
+    public void addDesignerMajor(DesignerMajor designerMajor) {
+        this.designerMajors.add(designerMajor);
+        designerMajor.updateDesigner(this);
+    }
 
     public int getPriceByMeetingType(MeetingType meetingType) {
         if (meetingType == MeetingType.OFFLINE) {
