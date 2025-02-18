7122c4f05bda65aedb1d24e2c0833e8ed65674b7
Jungsang Gi <wjdtkdrl15432@gmail.com>
2025-02-18 16:00:45 +0900

    permit 중복 수정

--- hairgg/src/main/java/hair/hairgg/security/SecurityConfig.java
+++ hairgg/src/main/java/hair/hairgg/security/SecurityConfig.java
@@ -27,7 +27,6 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/loginAuth").permitAll()
-                        .requestMatchers("/signup").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
