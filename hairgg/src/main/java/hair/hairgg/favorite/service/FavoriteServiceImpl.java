20acf18a1d7c3139e07373f657b2c669210975a5
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-19 13:42:04 +0900

    feat : 디자이너 찜, 찜 목록 조회 로직 구현

---
+++ hairgg/src/main/java/hair/hairgg/favorite/service/FavoriteServiceImpl.java
@@ -0,0 +1,56 @@
+package hair.hairgg.favorite.service;
+
+import hair.hairgg.designer.domain.Designer;
+import hair.hairgg.designer.service.DesignerService;
+import hair.hairgg.favorite.domain.Favorite;
+import hair.hairgg.favorite.repository.FavoriteRepository;
+import hair.hairgg.member.Member;
+import hair.hairgg.member.MemberRepository;
+import hair.hairgg.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class FavoriteServiceImpl implements FavoriteService {
+
+    private final DesignerService designerService;
+
+    private final MemberRepository memberRepository;
+    private final FavoriteRepository favoriteRepository;
+
+
+    @Override
+    @Transactional
+    public Long checkFavoriteDesigner(Long memberId, Long designerId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new RuntimeException("No member")
+        );
+
+        Designer designer = designerService.getDesignerById(designerId);
+        Favorite favorite = favoriteRepository.findFavoriteByMemberIdAndDesignerId(memberId, designerId);
+
+        if (favorite == null) {
+            favorite = Favorite.builder()
+                    .member(member)
+                    .designer(designer)
+                    .build();
+            return favoriteRepository.save(favorite).getId();
+        }
+
+        favoriteRepository.delete(favorite);
+        return null;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Designer> getFavoriteDesigners(Long memberId, Integer page) {
+        Pageable pageable = PageRequest.of(page, 30, Sort.by("id").ascending());
+        return favoriteRepository.findDesignerByMemberId(memberId, pageable);
+    }
+}
