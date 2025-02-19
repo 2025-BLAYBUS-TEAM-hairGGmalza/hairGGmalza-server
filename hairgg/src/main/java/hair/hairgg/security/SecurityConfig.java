10ffb5a672371b56e212ce862a4c1d5bcd0f83c4
Miging <dnfldpden32@gmail.com>
2025-02-19 18:15:25 +0900

    chore: 개발용으로 모든 경로 토큰 없이 허용

--- hairgg/src/main/java/hair/hairgg/security/SecurityConfig.java
+++ hairgg/src/main/java/hair/hairgg/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/loginAuth", "/designers", "/designers/**", "/reviews").permitAll()
+                        .requestMatchers("/login", "/loginAuth", "/designers", "/designers/**", "/reviews","/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
